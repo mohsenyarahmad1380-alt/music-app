249172b2400e3b60dc859451c45506af638d0614
mohsen yarahmad <mohsenyarahmad1380@gmail.com>
2026-09-17 15:01:18 +0330

    init: vite + react + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-lg font-bold text-blue-600">تست تیلویند</h1>
+    </>
+  );
+}
+
+export default App;
